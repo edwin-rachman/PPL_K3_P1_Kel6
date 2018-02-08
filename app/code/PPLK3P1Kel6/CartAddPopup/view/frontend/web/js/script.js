a41056728b9a78727731f6c678ce172798390e6a
edwin-rachman <13515042@std.stei.itb.ac.id>
2018-02-09 05:50:58 +0700

    Implement set max limit button

--- app/code/PPLK3P1Kel6/CartAddPopup/view/frontend/web/js/script.js
+++ app/code/PPLK3P1Kel6/CartAddPopup/view/frontend/web/js/script.js
@@ -2,12 +2,29 @@ require([
     'jquery',
     'underscore',
     'Magento_Ui/js/modal/alert',
+    'Magento_Ui/js/modal/prompt',
     'jquery/jquery-storageapi'
-], function($, _, alertWidget) {
+], function($, _, alertWidget, promptWidget) {
   var storage = $.initNamespaceStorage('mage-cache-storage').localStorage;
   var cartLimit = storage.get('cart-limit');
   cartLimit = (typeof cartLimit === 'undefined' || cartLimit === null) ? Infinity : cartLimit;
-  $('.minicart-wrapper').prepend($('<button/>').text('Max limit: $' + cartLimit).click(function() {}));
+  
+  $('.showcart').before($('<button/>').text(cartLimit === Infinity ? 'No max limit' : ('Max limit: $' + cartLimit)).click(function() {
+    var $this = $(this);
+    promptWidget({
+      title: 'Set maximum cart total limit',
+      content: 'Enter new maximum cart total limit below (in $): ',
+      value: cartLimit,
+      actions: {
+        confirm: function(newValue) {
+          cartLimit = parseInt(newValue, 10);
+          cartLimit = isNaN(cartLimit) ? Infinity : cartLimit;
+          storage.set('cart-limit', cartLimit);
+          $this.text(cartLimit === Infinity ? 'No max limit' : ('Max limit: $' + cartLimit));
+        }
+      }
+    });
+  }));
   
   $(document).ajaxComplete(function (event, xhr, settings) {
     if (settings.url.match(/customer\/section\/load/i) && xhr.responseJSON && xhr.responseJSON.cart) {
@@ -19,37 +36,41 @@ require([
       var oldCartItems = {};
       $.each(oldCart.items, function(){oldCartItems[this.item_id] = this;});
 
-      cartTotal = 0
+      productAdded = null;
+      productPrice = null;
+      cartTotal = 0;
       $.each(newCart.items, function(index, item){
         cartTotal += item.product_price_value * item.qty;
         var id = this.item_id;
         if (typeof oldCartItems[id] !== 'undefined') {
           var oldQty = oldCartItems[id].qty;
-          if (oldQty < this.qty) {
-            this.qty -= oldQty;
-            addedItems[id] = this;
-          } else if (oldQty > this.qty) {
-            this.qty = oldQty - this.qty;
+          if (oldQty < this.qty && productAdded === null) {
+            productAdded = item.product_name;
+            productPrice = item.product_price_value;
           }
           delete oldCartItems[id];
-        } else {
-          addedItems[id] = this;
+        } 
+        else if (productAdded === null) {
+          productAdded = item.product_name;
+          productPrice = item.product_price_value;
         }
       });
       
-      if (_.size(addedItems) > 0) {
-        $.each(addedItems, function(id, item) {
+      if (productAdded !== null) {
+        if (cartTotal > cartLimit) {
           alertWidget({
-            title: 'Product added to cart' 
-            + (cartTotal > cartLimit ? ' - Maximum cart total limit exceeded' : ''),
-            content: 'Added product: ' + item.product_name
-            + ' (Price: $' + item.product_price_value + ').'
-            + (cartTotal > cartLimit ? (' Cart total ($' + cartTotal + ') has exceeded maximum limit set by user ($' + cartLimit + ').') : '')
+            title: 'Add to Cart - Maximum cart total limit exceeded',
+            content: productAdded + ' successfully added to cart (Price: $' + productPrice + '). Cart total ($' + cartTotal + ') has exceeded maximum limit set by user ($' + cartLimit + ').'
           });
-        });
+        }
+        else {
+          alertWidget({
+            title: 'Add to Cart',
+            content: productAdded + ' successfully added to cart (Price: $' + productPrice + ').'
+          });
+        }
       }
       storage.set('old-cart', newCart);
-      storage.set('cart-limit', cartLimit);
     }
   });
 });

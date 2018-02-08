80e2c31a6cc4af1ae1ad64edbb3df3a8f4236abd
edwin-rachman <13515042@std.stei.itb.ac.id>
2018-02-08 18:05:09 +0700

    Add maximum cart limit warning to popup

--- app/code/PPLK3P1Kel6/CartAddPopup/view/frontend/web/js/script.js
+++ app/code/PPLK3P1Kel6/CartAddPopup/view/frontend/web/js/script.js
@@ -3,53 +3,52 @@ require([
     'underscore',
     'Magento_Ui/js/modal/alert',
     'jquery/jquery-storageapi'
-], function($, _, alert) {
-    var storage = $.initNamespaceStorage('mage-cache-storage').localStorage;
+], function($, _, alertWidget) {
+  var storage = $.initNamespaceStorage('mage-cache-storage').localStorage;
   
-    $(document).ajaxComplete(function (event, xhr, settings) {
-        if (settings.url.match(/customer\/section\/load/i) && xhr.responseJSON && xhr.responseJSON.cart) {
-            var addedItems = {};
-            var removedItems = {};
+  $(document).ajaxComplete(function (event, xhr, settings) {
+    if (settings.url.match(/customer\/section\/load/i) && xhr.responseJSON && xhr.responseJSON.cart) {
+      var addedItems = {};
+      var newCart = xhr.responseJSON.cart;
+      var oldCart = storage.get('old-cart');
+      var cartLimit = storage.get('cart-limit');
+      oldCart = (typeof oldCart === 'undefined') ? {'items':[]} : oldCart;
+      cartLimit = (typeof cartLimit === 'undefined') ? Infinity : cartLimit;
 
-            var newCart = xhr.responseJSON.cart;
-            var oldCart = storage.get('iwd-old-cart');
-            oldCart = (typeof oldCart === 'undefined') ? {'items':[]} : oldCart;
+      var oldCartItems = {};
+      $.each(oldCart.items, function(){oldCartItems[this.item_id] = this;});
 
-            var oldCartItems = {};
-            $.each(oldCart.items, function(){oldCartItems[this.item_id] = this;});
-
-            $.each(newCart.items, function(item){
-                var id = this.item_id;
-                if (typeof oldCartItems[id] !== 'undefined') {
-                    var oldQty = oldCartItems[id].qty;
-                    if (oldQty < this.qty) {
-                        this.qty -= oldQty;
-                        addedItems[id] = this;
-                    } else if (oldQty > this.qty) {
-                        this.qty = oldQty - this.qty;
-                        removedItems[id] = this;
-                    }
-                    delete oldCartItems[id];
-                } else {
-                    addedItems[id] = this;
-                }
-            });
-
-            $.each(oldCartItems, function(id, item){removedItems[id] = item;});
-
-            if (_.size(addedItems) > 0) {
-              $.each(addedItems, function(id, item) {
-                alert({
-                  title: 'Product added to cart',
-                  content: 'Added product: ' + item.product_name + ' (Price: $' + item.product_price_value + ')'
-                });
-              });
-            }
-            //if (_.size(removedItems) > 0) {
-            //    $('body').trigger('productRemoved', [{items:removedItems}]);
-            //}
-
-            storage.set('iwd-old-cart', newCart);
+      cartTotal = 0
+      $.each(newCart.items, function(index, item){
+        cartTotal += item.product_price_value * item.qty;
+        var id = this.item_id;
+        if (typeof oldCartItems[id] !== 'undefined') {
+          var oldQty = oldCartItems[id].qty;
+          if (oldQty < this.qty) {
+            this.qty -= oldQty;
+            addedItems[id] = this;
+          } else if (oldQty > this.qty) {
+            this.qty = oldQty - this.qty;
+          }
+          delete oldCartItems[id];
+        } else {
+          addedItems[id] = this;
         }
-    });
+      });
+      
+      if (_.size(addedItems) > 0) {
+        $.each(addedItems, function(id, item) {
+          alertWidget({
+            title: 'Product added to cart' 
+            + (cartTotal > cartLimit ? ' - Maximum cart total limit exceeded' : ''),
+            content: 'Added product: ' + item.product_name
+            + ' (Price: $' + item.product_price_value + ').'
+            + (cartTotal > cartLimit ? (' Cart total ($' + cartTotal + ') has exceeded maximum limit set by user ($' + cartLimit + ').') : '')
+          });
+        });
+      }
+      storage.set('old-cart', newCart);
+      storage.set('cart-limit', cartLimit);
+    }
+  });
 });
